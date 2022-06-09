57f11089e69a3a18b1408290eeb95266b715fc4f
Wellyngton Antonio Kuhn <94876959+Wellyngtonkuhn@users.noreply.github.com>
2022-06-09 17:15:34 -0300

    Finalizando a Home

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,47 @@
+import Image from "next/image";
 
-import styles from '../styles/Home.module.css'
+import Card from "./componentes/Card";
 
+import styles from "../styles/Home.module.css";
 
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = "https://pokeapi.co/api/v2/pokemon";
 
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
 
-export default function Home() {
+  // adiciona o index do pokemon
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
   return (
     <>
-    <h1>Home</h1>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>
+          Poke<span>Next</span>
+        </h1>
+        <Image
+          src={"/imagens/pokeball.png"}
+          width={50}
+          height={50}
+          alt="PokeNext"
+        />
+      </div>
+      <div className={styles.pokemon_container}>
+        {pokemons.map((pokemon) => (
+          <Card pokemon={pokemon} key={pokemon.id}/>
+        ))}
+      </div>
     </>
-  )
+  );
 }
